cf71b00d42192e3123e7efff26277d994e4b2f7e
ashwin-pc <ashwinpc1993@gmail.com>
2017-02-15 01:28:00 +0530

    small fixes

--- game.js
+++ game.js
@@ -71,37 +71,6 @@ function Game(){
         }
     }
 
-    // this.checkIfWon = function () {
-    //     var sumRow = 0 ,sumCol = 0, row = 0, col = 0, dia = 0;
-
-    //     for (var i = 0; i < 3; i++) {
-    //         var sum = Math.abs(this.results[i][0] + this.results[i][1] + this.results[i][2]);
-    //         row = (sum > sumRow) ? i+1 : row;
-    //         sumRow = (sum > sumRow) ? sum : sumRow;
-    //         sum = Math.abs(this.results[0][i] + this.results[1][i] + this.results[2][i]);
-    //         col = (sum > sumCol) ? i+1 : col;
-    //         sumCol = (sum > sumCol) ? sum : sumCol;
-    //     }
-    //     var sumD1 = this.results[0][0] + this.results[1][1] + this.results[2][2];
-    //     var sumD2 = this.results[0][2] + this.results[1][1] + this.results[2][0];
-
-    //     if (sumRow === 3 || sumRow === -3 || sumCol === 3 || sumCol === -3 || sumD1 === 3 || sumD1 === -3 || sumD2 === 3 || sumD2 === -3) {
-    //         resultEle.innerHTML = (this.xTurn) ? "Player 1 Won the game" : "Player 2 Won the game";
-    //         this.updateScore(this.xTurn);
-    //         this.turnsLeft = -1;
-    //         row = (sumRow === 3 || sumRow === -3) ? row : null;
-    //         col = (sumCol === 3 || sumCol === -3) ? col : null;
-    //         if (sumD1 === 3 || sumD1 === -3) { dia = 1; } else if (sumD2 === 3 || sumD2 === -3) { dia = 2; } else { dia = null; };
-    //         setTimeout (function(){draw.winningLine(col,row,dia);},200);
-    //         return;
-    //     }
-
-    //     if (this.turnsLeft === 0) {
-    //         resultEle.innerHTML = "No More turns Left";
-    //         return;
-    //     }
-    // }
-
     this.hasWinner = function () {
         var data = this.getBoardData();
 
